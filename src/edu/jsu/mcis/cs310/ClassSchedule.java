adc7a635b71c74efc16e25541dbd5abeea333d21
Tremendes Roper <troper1@stu.jsu.edu>
2024-09-21 06:33:47 -0500

    V2 - Error message needing to be investigated

--- src/edu/jsu/mcis/cs310/ClassSchedule.java
+++ src/edu/jsu/mcis/cs310/ClassSchedule.java
@@ -5,12 +5,14 @@ import com.opencsv.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 public class ClassSchedule {
     
@@ -33,49 +35,129 @@ public class ClassSchedule {
     private final String SUBJECTID_COL_HEADER = "subjectid";
     
     public String convertCsvToJsonString(List<String[]> csv) {
+        
+        // Step 1: Create objects
+        JsonObject mainObj = new JsonObject();
+        JsonObject scheduleTypeObj = new JsonObject();
+        JsonObject subjectObj = new JsonObject();
+        JsonObject courseObj = new JsonObject();
+        JsonObject sectionObj = new JsonObject();
+        JsonArray sectionArray = new JsonArray();
+        
+        
+        // Step 2: Create HashMaps for special headers
+        HashMap<String, String> scheduleTypeMap = new HashMap<>();
+        HashMap<String, String> subjectMap = new HashMap<>();
        
-        // Objects needed for separation
         
-        JsonArray jArray = new JsonArray();
         
-        // Check to see if csv equal anything and if not will return empty array
-        if (csv == null || csv.isEmpty()){
-            return jArray.toString();
+        // Step 3: Separate the columns 
+        for (int i = 1; i < csv.size(); i++){
+            String[] column = csv.get(i);
+            
+            String crn = column[0];
+            String subject = column[1];
+            String num = column[2];
+            String description = column[3];
+            String section = column[4];
+            String scheduleTypeAbbreviation = column[5];
+            String[] parts = num.split(" ");
+            String courseKey = parts[0];
+            String courseNum = parts[1]; 
+            String credits = column[6];
+            String start = column[7];
+            String end = column[8];
+            String days = column[9];
+            String where = column[10];
+            String scheduleTypeFull = column[11];
+            String instructor = column[12];
+            
+                    
+            
+            // Object for Course Entry
+            JsonObject courseDetails = new JsonObject();
+            courseDetails.put("subjectid", courseKey);
+            courseDetails.put("num", courseNum);
+            courseDetails.put("description", description);
+            courseDetails.put("credits", credits);
+             
+            // Merging the two together for the course section details
+            courseObj.put(num, courseDetails);
+            
+            // Object for Section Entry
+            JsonObject sectionDetails = new JsonObject();
+            sectionDetails.put("crn", crn);
+            System.out.println(sectionDetails);
+            sectionDetails.put("subjectid", courseKey);
+            System.out.println(sectionDetails);
+            sectionDetails.put("num", courseNum);
+            sectionDetails.put("section", section);
+            sectionDetails.put("type", scheduleTypeAbbreviation);
+            sectionDetails.put("start", start);
+            sectionDetails.put("end", end);
+            sectionDetails.put("days", days);
+            sectionDetails.put("where", where);
+            
+            // Instructor separation
+            JsonArray instructorArray = new JsonArray();
+            instructorArray.add(instructor);
+            sectionObj.put("instructor", instructorArray);
+            // Merging the two together
+            sectionObj.put(num, sectionDetails);
+            
+            sectionArray.add(sectionObj);
+            
+            // Checks for scheduletype and subject
+            if (!scheduleTypeMap.containsKey(scheduleTypeAbbreviation)){
+                scheduleTypeMap.put(scheduleTypeAbbreviation, scheduleTypeFull);
+            }
+            
+            if(!subjectMap.containsKey(subject)){
+                subjectMap.put(courseKey, subject);
+            }
+            
+            
+            
         }
         
-        // Suppose to assume the first row is the headers
-        String [] headers = csv.get(0);
+        // Map the Schedule Type Entry 
+        for (Map.Entry<String, String> entry : scheduleTypeMap.entrySet()){
+            scheduleTypeObj.put(entry.getKey(), entry.getValue());
+        }
         
-        // Iteration through csv rows
-        for (String[] row : csv.subList(1, csv.size())){
-            HashMap<String, String> rowM = new HashMap<>();
+        // Map the Subject Entry
+        for(Map.Entry<String, String> entry : subjectMap.entrySet()){
+            subjectObj.put(entry.getKey(), entry.getValue());
+        }
             
-            // Iteration for headers and rows
-            Iterator<String> headerIterator = List.of(headers).iterator();
-            Iterator<String> rowIterator = List.of(row).iterator();
+        // Adding the section headers with their details to main object
+        
+        System.out.println(sectionArray);
+        mainObj.put("scheduletype", scheduleTypeObj);
+        mainObj.put("subject", subjectObj);
+        mainObj.put("course", courseObj);
+        mainObj.put("section", sectionArray);
+        
+        
+        
+        return mainObj.toString();
 
-            while (headerIterator.hasNext() && rowIterator.hasNext()){
-                rowM.put(headerIterator.next(), rowIterator.next());
-            }
-            
-            
-            // Conversion into JsonObject and placed in JsonArray
-            
-            JsonObject jObject = new JsonObject();
-            for (String key : rowM.keySet()){
-                jObject.put(key, rowM.get(key));
-            }
-            
-            jArray.add(jObject);
-        }
-        
-        return jArray.toString();
     }
     
     public String convertJsonToCsvString(JsonObject json) {
+        // Step 1:
         
-        return ""; // remove this!
+        // Step 2:
         
+        // Step 3:
+        
+        // Step 4:
+        
+        // Step 5:
+        
+        // Step 6:
+        
+        return "";
     }
     
     public JsonObject getJson() {
